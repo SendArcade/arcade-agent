14f53f6c94db621408179e71cc8835a789565add
Arpit Singh Bhatia <84636859+The-x-35@users.noreply.github.com>
2024-12-27 00:59:17 +0530

    prod

--- src/app/solana-agent-kit/langchain/index.ts
+++ src/app/solana-agent-kit/langchain/index.ts
@@ -786,7 +786,7 @@ export class SolanaRockPaperScissorsTool extends Tool {
         Number(parsedInput['"amount"']),
         parsedInput['"choice"'].replace(/^"|"$/g, '') as "rock" | "paper" | "scissors"
       );
-      return "https://raw.githubusercontent.com/The-x-35/rps-solana-blinks/refs/heads/main/public/1.jpeg\n"+result;
+      return "https://raw.githubusercontent.com/The-x-35/rps-solana-blinks/refs/heads/main/public/1.jpeg\n"+ await result;
     } catch (error: any) {
       return "Sorry an error occurred. Please try again later.";
     }
